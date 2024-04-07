4d8ff2711eb3953370f5e54cb807d99870019681
Walter Fritsche <wfritsche95@protonmail.com>
2024-04-07 11:45:43 -0500

    added GET /scoreboard controller

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,4 +1,6 @@
 const asyncHandler = require("express-async-handler");
+const Scores = require("../models/Scores");
+
 module.exports.GET_GAMEIMAGE = asyncHandler(async (req, res) => {
   res.send("route /gameimage has not been handled yet");
 });
@@ -8,7 +10,16 @@ module.exports.POST_VERIFYCLICK = asyncHandler(async (req, res) => {
 });
 
 module.exports.GET_SCOREBOARD = asyncHandler(async (req, res) => {
-  res.send("GET /scoreboard has not been implemented yet.");
+  let limit = parseInt(req.query.limit) || 10;
+  if (limit > 50) {
+    limit = 50;
+  }
+  const scores = await Scores.find({})
+    .sort({ scoreMillis: "asc" })
+    .limit(limit)
+    .exec();
+  const numOfScores = scores.length;
+  res.json({ success: true, topScores: scores, numOfScores: numOfScores });
 });
 
 module.exports.POST_SCOREBOARD = asyncHandler(async (req, res) => {
