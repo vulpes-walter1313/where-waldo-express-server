30761d54f4101ffaad7ea305de1a53bcb443c055
Walter Fritsche <wfritsche95@protonmail.com>
2024-04-13 16:11:36 -0500

    added POST /scoreboard

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -16,7 +16,6 @@ module.exports.GET_GAMEIMAGE = [
 
     if (valResult.isEmpty()) {
       const { imageCode } = matchedData(req);
-      console.log(`imageCode is ${imageCode}`);
       if (imageCode === "1" || imageCode === "2") {
         const imageInfo = await GameImage.findOne({
           name: `waldo-${imageCode}`,
@@ -26,6 +25,7 @@ module.exports.GET_GAMEIMAGE = [
         req.session.endTime = null;
         req.session.waldoFound = false;
         req.session.wizardFound = false;
+        req.session.scoreSubmitted = false;
         return res.json({ success: true, imageUrl: imageInfo.imgUrl });
       } else {
         // error here
@@ -62,7 +62,6 @@ module.exports.POST_VERIFYCLICK = [
         widthpx: clickWidthPx,
         heightpx: clickHeightPx,
       } = matchedData(req);
-      console.log(`character: ${charName}\nxCoords: ${clickXCoord}\nyCoordS`);
       // get game being played
       const game = await GameImage.findOne({
         name: req.session.gameName,
@@ -150,6 +149,69 @@ module.exports.GET_SCOREBOARD = asyncHandler(async (req, res) => {
   res.json({ success: true, topScores: scores, numOfScores: numOfScores });
 });
 
-module.exports.POST_SCOREBOARD = asyncHandler(async (req, res) => {
-  res.send("POST /scoreboard has not been implemented yet.");
-});
+module.exports.POST_SCOREBOARD = [
+  body("username").isAlphanumeric().isLength({ max: 40 }),
+  asyncHandler(async (req, res) => {
+    const valResult = validationResult(req);
+    if (valResult.isEmpty()) {
+      const timeScoreMillis = req.session.endTime - req.session.startTime;
+      if (req.session.scoreSubmitted) {
+        const error = new Error("Score already submitted");
+        error.status = 400;
+        throw error;
+      }
+      const { username } = matchedData(req);
+
+      // check if less than 50 scores are in db
+      const quantityOfScores = await Scores.countDocuments({}).exec();
+      if (quantityOfScores < 50) {
+        const newScore = new Scores({
+          username: username ?? "Anonymous",
+          scoreMillis: timeScoreMillis,
+        });
+        await newScore.save();
+        req.session.scoreSubmitted = true;
+        return res.json({
+          success: true,
+          isTopScore: true,
+          score: newScore.scoreMillis,
+        });
+      } else {
+        // if 50 then check if new score beats the last score.
+        const lastScore = (
+          await Scores.find({}).sort({ scoreMillis: "desc" }).limit(1).exec()
+        )[0];
+        console.log("last score:\n");
+        console.log(lastScore);
+        if (lastScore.scoreMillis > timeScoreMillis) {
+          await Scores.findByIdAndDelete(lastScore._id);
+          const newScore = new Scores({
+            username: username ?? "Anonymous",
+            scoreMillis: timeScoreMillis,
+          });
+          await newScore.save();
+          req.session.scoreSubmitted = true;
+          return res.json({
+            success: true,
+            isTopScore: true,
+            score: newScore.scoreMillis,
+          });
+        } else {
+          // score not in top 50
+          req.session.scoreSubmitted = true;
+          return res.json({
+            success: true,
+            isTopScore: false,
+            score: timeScoreMillis,
+          });
+        }
+        // res.json({success: tr})
+      }
+
+      // if newscore beats last score, drop last score
+      // and insert new score.
+    } else {
+      return res.status(400).json({ success: false, error: valResult.array() });
+    }
+  }),
+];
