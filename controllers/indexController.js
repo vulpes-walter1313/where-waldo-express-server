4768834f58b77056eda42d5b30efabf071a8a5c3
Walter Fritsche <wfritsche95@protonmail.com>
2024-04-13 15:01:12 -0500

    implemented verifyclick

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,7 +1,13 @@
 const asyncHandler = require("express-async-handler");
 const Scores = require("../models/Scores");
 const GameImage = require("../models/GameImages");
-const { query, matchedData, validationResult } = require("express-validator");
+const CharacterCoords = require("../models/CharacterCoords");
+const {
+  query,
+  body,
+  matchedData,
+  validationResult,
+} = require("express-validator");
 
 module.exports.GET_GAMEIMAGE = [
   query("imageCode").notEmpty(),
@@ -33,9 +39,103 @@ module.exports.GET_GAMEIMAGE = [
   }),
 ];
 
-module.exports.POST_VERIFYCLICK = asyncHandler(async (req, res) => {
-  res.send("route POST /verifyclick has not been implemented yet");
-});
+module.exports.POST_VERIFYCLICK = [
+  body("character").custom((value) => {
+    if (value === "waldo" || value === "wizard") {
+      return true;
+    } else {
+      throw new Error("Not a valid character in verifyclick body");
+    }
+  }),
+  body("xCoord").isNumeric(),
+  body("yCoord").isNumeric(),
+  body("widthpx").isNumeric(),
+  body("heightpx").isNumeric(),
+  asyncHandler(async (req, res) => {
+    const valResult = validationResult(req);
+
+    if (valResult.isEmpty()) {
+      const {
+        character: charName,
+        xCoord: clickXCoord,
+        yCoord: clickYCoord,
+        widthpx: clickWidthPx,
+        heightpx: clickHeightPx,
+      } = matchedData(req);
+      console.log(`character: ${charName}\nxCoords: ${clickXCoord}\nyCoordS`);
+      // get game being played
+      const game = await GameImage.findOne({
+        name: req.session.gameName,
+      }).exec();
+
+      // get character coords for the character of the game above.
+      const character = await CharacterCoords.findOne({
+        gameName: game,
+        character: charName,
+      }).exec();
+
+      // verify click and update session data
+      const characterRelX = character.x / game.widthpx;
+      const characterRelY = character.y / game.heightpx;
+      const characterXVarience = Math.abs(
+        characterRelX - (character.x + character.xRange) / game.widthpx,
+      );
+      const characterYVarience = Math.abs(
+        characterRelY - (character.y + character.yRange) / game.heightpx,
+      );
+
+      const clickRelX = clickXCoord / clickWidthPx;
+      const clickRelY = clickYCoord / clickHeightPx;
+
+      const characterXFound =
+        Math.abs(clickRelX - characterRelX) < characterXVarience;
+      const characterYFound =
+        Math.abs(clickRelY - characterRelY) < characterYVarience;
+
+      const characterFound = characterXFound && characterYFound;
+
+      if (characterFound) {
+        if (character.character === "waldo") {
+          req.session.waldoFound = true;
+        } else if (character.character === "wizard") {
+          req.session.wizardFound = true;
+        }
+        // check if game won
+        if (req.session.waldoFound && req.session.wizardFound) {
+          // game won
+          req.session.endTime = Date.now();
+          const time = req.session.endTime - req.session.startTime;
+          const scores = await Scores.find({})
+            .sort({ scoreMillis: "asc" })
+            .exec();
+          const newTopScore = time < scores[scores.length - 1].scoreMillis;
+          return res.json({
+            success: true,
+            clickIsCorrect: true,
+            wonGame: true,
+            isTopScore: newTopScore,
+            score: time,
+          });
+        } else {
+          // character found but game not won
+          return res.json({
+            success: true,
+            clickIsCorrect: true,
+            wonGame: false,
+          });
+        }
+      } else {
+        // character not found, we'll get 'em next time.
+        res.json({ success: true, clickIsCorrect: false, wonGame: false });
+      }
+    } else {
+      // json body validation failed
+      res
+        .status(400)
+        .json({ success: false, error: valResult.array(), status: 400 });
+    }
+  }),
+];
 
 module.exports.GET_SCOREBOARD = asyncHandler(async (req, res) => {
   let limit = parseInt(req.query.limit) || 10;
