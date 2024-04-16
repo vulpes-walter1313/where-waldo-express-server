2d195b04513a66c357bb419da85742a062ca9f02
Walter Fritsche <wfritsche95@protonmail.com>
2024-04-16 11:22:51 -0500

    added which game got the score

--- models/Scores.js
+++ models/Scores.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const scoresSchema = mongoose.Schema({
   username: String,
   scoreMillis: Number,
+  game: {
+    type: mongoose.ObjectId,
+    ref: "gameimage",
+  },
 });
 
 const Scores = mongoose.model("scores", scoresSchema);
